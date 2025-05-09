a48aadb1f6b45096ab65fe14322cbf89ee04c797
Illia Berezin <berezin.main@gmail.com>
2025-05-09 15:15:38 +0300

    React Main Project

---
+++ frontend/src/components/FirstComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class FirstComponent extends React.Component {
+
+    render() {
+        return (
+            <div>
+                Hello from React Component!
+            </div>
+        )
+    }
+}
+
+export default FirstComponent;
